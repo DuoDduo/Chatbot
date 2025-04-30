72612adad109ae46b871a039dff1e4ca90b59e3a
maverick <pelubs2956@gmail.com>
2025-04-30 23:12:11 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function sendMessage(){
+const userMessage = document.querySelector('.chat-window input').value
+
+alert(userMessage);
+
+}
+document.querySelector('.chat-window .input-area button').addEventListener("click", ()=> sendMessage())
